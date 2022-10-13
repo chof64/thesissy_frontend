0852be9a313a2ebbd8bd6890f0d140a2c74fb1b7
Chad Fernandez <chadfernandez@duck.com>
2022-10-13 20:03:17 +0800

    added navigation to default layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,7 +3,8 @@ import Navigation from "./Navigation";
 export default function Layout({ children }) {
   return (
     <main className="subpixel-antialiased">
-      <div>
+      <div className="w-full h-2 bg-wmsu-red" />
+      <div className="sticky top-0 z-[100]">
         <Navigation />
       </div>
       <div>{children}</div>
