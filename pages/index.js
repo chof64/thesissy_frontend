29d0f130d6ef1183909295029520c54ba041b779
Chad Fernandez <chadfernandez@duck.com>
2022-10-13 21:47:41 +0800

    fixed hero error with banner and buttons

--- pages/index.js
+++ pages/index.js
@@ -9,32 +9,36 @@ export default function Index() {
   return (
     <>
       <Platform className="mt-7">
-        <div className="flex items-center justify-center w-full">
+        <div className="flex w-full flex-col items-center justify-center">
           <div className="h-64 w-full rounded-md bg-[url('/images/hero-background-image.jpg')] bg-cover">
-            <div className="flex flex-col items-center justify-center w-full h-full rounded-md bg-red-900/50">
-              <h1 className="text-xl font-bold text-center text-white">
+            <div className="flex h-full w-full flex-col items-center justify-center rounded-md bg-red-900/50">
+              <h1 className="text-center text-xl font-bold text-white">
                 WMSU HUMAN RESOURCES INFORMATION{" "}
                 <span className="text-wmsu-red">SYSTEM</span>
               </h1>
-              <p className="mt-3 text-sm text-center text-white">
+              <p className="mt-3 text-center text-sm text-white">
                 Access all available data in just one click.
               </p>
             </div>
-            <div className="flex flex-col gap-2 mt-2">
-              <Link href="/#login">
-                <button className="inline-flex items-center w-full p-2 font-semibold text-white rounded-md h-14 bg-wmsu-red">
-                  <LogInIcon className="mr-1 aspect-square h-5 stroke-[3]" />
-                  Login
-                </button>
-              </Link>
-              <Link href="/#learnmore">
-                <button className="inline-flex items-center w-full p-2 font-semibold border rounded-md h-14">
-                  <InfoIcon className="mr-1 aspect-square h-5 stroke-[3]" />
-                  Learn More
-                </button>
-              </Link>
-            </div>
           </div>
+          <div className="mt-2 flex w-full flex-col gap-2">
+            <Link href="/#login">
+              <button className="inline-flex h-14 w-full items-center rounded-md bg-wmsu-red p-2 font-semibold text-white">
+                <LogInIcon className="mr-1 aspect-square h-5 stroke-[3]" />
+                Login
+              </button>
+            </Link>
+            <Link href="/#learnmore">
+              <button className="inline-flex h-14 w-full items-center rounded-md border p-2 font-semibold">
+                <InfoIcon className="mr-1 aspect-square h-5 stroke-[3]" />
+                Learn More
+              </button>
+            </Link>
+          </div>
+        </div>
+        <div>
+          <h1>Abstract</h1>
+          <p>Abstract Content</p>
         </div>
       </Platform>
     </>
