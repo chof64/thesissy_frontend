7317bab8a062de5c1eccfe1d9a7aa496f6fb6263
Chad Fernandez <chadfernandez@duck.com>
2022-10-13 13:54:19 +0800

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "wmsu-red": "#A20202",
+        "app-white": "#EBD8D8",
+        "app-red-gray": "#4F4141",
+      },
+      fontFamily: {
+        // TODO: Add backup fonts
+        sans: ["Poppins"],
+      },
+    },
+  },
+  plugins: [],
+};
