9c7a0dbf33b1248b72f07d01209598f02e9bde73
Chad Fernandez <chadfernandez@duck.com>
2022-10-13 20:03:36 +0800

    formatted `index.js` with prettier

--- pages/index.js
+++ pages/index.js
@@ -10,26 +10,26 @@ export default function Index() {
     <>
       <Platform className="mt-7">
         <div className="flex items-center justify-center w-full">
-          <div className="bg-[url('/images/hero-background-image.jpg')] bg-cover w-full h-64 rounded-md">
-            <div className="bg-red-900/50 rounded-md h-full w-full flex flex-col justify-center items-center">
-              <h1 className="font-bold text-xl text-center text-white">
+          <div className="h-64 w-full rounded-md bg-[url('/images/hero-background-image.jpg')] bg-cover">
+            <div className="flex flex-col items-center justify-center w-full h-full rounded-md bg-red-900/50">
+              <h1 className="text-xl font-bold text-center text-white">
                 WMSU HUMAN RESOURCES INFORMATION{" "}
                 <span className="text-wmsu-red">SYSTEM</span>
               </h1>
-              <p className="mt-3 text-center text-sm text-white">
+              <p className="mt-3 text-sm text-center text-white">
                 Access all available data in just one click.
               </p>
             </div>
-            <div className="mt-2 flex flex-col gap-2">
+            <div className="flex flex-col gap-2 mt-2">
               <Link href="/#login">
-                <button className="inline-flex items-center w-full p-2 h-14 rounded-md font-semibold bg-wmsu-red text-white">
-                  <LogInIcon className="h-5 mr-1 aspect-square stroke-[3]" />
+                <button className="inline-flex items-center w-full p-2 font-semibold text-white rounded-md h-14 bg-wmsu-red">
+                  <LogInIcon className="mr-1 aspect-square h-5 stroke-[3]" />
                   Login
                 </button>
               </Link>
               <Link href="/#learnmore">
-                <button className="inline-flex items-center w-full p-2 h-14 rounded-md font-semibold border">
-                  <InfoIcon className="h-5 mr-1 aspect-square stroke-[3]" />
+                <button className="inline-flex items-center w-full p-2 font-semibold border rounded-md h-14">
+                  <InfoIcon className="mr-1 aspect-square h-5 stroke-[3]" />
                   Learn More
                 </button>
               </Link>
