58fea63d50fac657bd1a89c5cd2544ec8112d1bb
marymaeerasga <marymaeerasga30@gmail.com>
2022-10-15 09:01:08 +0800

    edited the nav

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,7 +4,7 @@ export default function Layout({ children }) {
   return (
     <main className="subpixel-antialiased">
       <div className="w-full h-2 bg-wmsu-red" />
-      <div className="sticky top-0 z-[100]">
+      <div className=" top-0 z-[100]">
         <Navigation />
       </div>
       <div>{children}</div>
