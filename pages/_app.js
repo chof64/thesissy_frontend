8613d5055b32a1b546e62e8cf09c1d1b081a1817
Chad Fernandez <chadfernandez@duck.com>
2022-10-16 15:57:07 +0800

    added supabase auth session in `_app.js`

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,30 @@
+import React from "react";
+import { useRouter } from "next/router";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+
 import "/src/styles/globals.css";
 
-export default function app({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
 
-  return getLayout(<Component {...pageProps} />);
+  const router = useRouter();
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+
+  return getLayout(
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
+      <button
+        onClick={async () => {
+          await supabaseClient.auth.signOut();
+          router.push("/");
+        }}
+      >
+        Logout
+      </button>
+      <Component {...pageProps} />
+    </SessionContextProvider>
+  );
 }
