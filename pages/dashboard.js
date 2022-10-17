04eb3824168c535687bab5c2656afdeaa6783895
marymaeerasga <marymaeerasga30@gmail.com>
2022-10-18 03:55:28 +0800

    edited dashboard

---
+++ pages/dashboard.js
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import LayoutGlobal from "/src/components/Layout/LayoutGlobal";
+import Layout from "/src/components/Layout/Layout";
+import Platform from "/src/components/Layout/Platform";
+import Link from "next/link";
+
+
+export default function Dashboard() {
+
+  return (
+    <>
+      <Platform className="mt-6">
+         <div class="container w-full px-8 py-8 overflow-hidden bg-gray-100">
+            <div className="">
+                <h1>Welcome, Admin!</h1>
+            </div>
+            <div className="container">
+                <div class="grid grid-cols-4 gap-4 md:grid-cols-4 ">
+                    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+                        <div class="px-6 py-4">
+                            <div class="font-bold text-xl mb-2">01</div>
+                        </div>
+                    </div>
+                    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+                        <div class="px-6 py-4">
+                            <div class="font-bold text-xl mb-2">01</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+         </div>
+            <div className="container my-1">
+                <div class="grid grid-cols-4 gap-4 ">
+                    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+                        <div class="px-6 py-4">
+                            <div class="font-bold text-xl mb-2">01</div>
+                        </div>
+                    </div>
+                    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+                        <div class="px-6 py-4">
+                            <div class="font-bold text-xl mb-2">01</div>
+                        </div>
+                    </div>
+                    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+                        <div class="px-6 py-4">
+                            <div class="font-bold text-xl mb-2">01</div>
+                        </div>
+                    </div>
+                    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+                        <div class="px-6 py-4">
+                            <div class="font-bold text-xl mb-2">01</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="container my-3">
+            <div class="grid grid-cols-3 gap-4">
+                    <div class="col-span-2 rounded overflow-hidden shadow-lg">
+                        <div class="px-6 py-4">
+                            <div class="font-bold text-xl mb-2">01</div>
+                        </div>
+                    </div>
+                    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+                        <div class="px-6 py-4">
+                            <div class="font-bold text-xl mb-2">01</div>
+                        </div>
+                    </div>
+                    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+                        <div class="px-6 py-4">
+                            <div class="font-bold text-xl mb-2">01</div>
+                        </div>
+                    </div>
+                    <div class="col-span-2 rounded overflow-hidden shadow-lg">
+                        <div class="px-6 py-4">
+                            <div class="font-bold text-xl mb-2">01</div>
+                        </div>
+                    </div>
+            </div>
+            </div>
+      </Platform>
+    </>
+  );
+}
+
+Dashboard.getLayout = function getLayout(page) {
+  return (
+    <LayoutGlobal title="Dashboard">
+      <Layout>{page}</Layout>
+    </LayoutGlobal>
+  );
+};
