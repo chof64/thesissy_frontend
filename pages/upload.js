0e7c0d1cb685c8b4a1e2ae4f2f97b3eef30d1991
marymaeerasga <marymaeerasga30@gmail.com>
2022-10-18 17:28:34 +0800

    update the upload page

---
+++ pages/upload.js
@@ -0,0 +1,36 @@
+import React from "react";
+import LayoutGlobal from "/src/components/Layout/LayoutGlobal";
+import Layout from "/src/components/Layout/Layout";
+import Platform from "/src/components/Layout/Platform";
+
+
+export default function Upload() {
+
+  return (
+    <>
+      <Platform className="mt-6">
+        <div className="container rounded w-full px-8 py-8 overflow-hidden bg-gray-100 flex flex-col items-center justify-center">
+            <div className="w-96">
+                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300" for="file_input">Select a file</label>
+                <input className="block w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="file_input_help" id="file_input" type="file"></input>
+                <p className="mt-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help">XLSX</p>
+            </div>
+            <div className="w-96 my-4">
+              <button className="items-center p-2 px-5 py-2 text-sm font-semibold text-white uppercase cursor-pointer text-md bg-wmsu-red hover:bg-red-700">
+                Upload
+              </button>
+            </div>
+          </div>
+
+      </Platform>
+
+    </>
+  );
+}
+Upload.getLayout = function getLayout(page) {
+  return (
+    <LayoutGlobal title="Upload">
+      <Layout>{page}</Layout>
+    </LayoutGlobal>
+  );
+};
