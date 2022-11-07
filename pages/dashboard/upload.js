b96f4100f20aac28e102acf7ed4b4c5987084f8f
Chad Fernandez <chadfernandez@duck.com>
2022-11-07 21:22:16 +0800

    working supabase storage upload

--- pages/dashboard/upload.js
+++ pages/dashboard/upload.js
@@ -1,35 +1,19 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import { Tab } from "@headlessui/react";
 
 import LayoutGlobal from "/src/components/Layout/LayoutGlobal";
 import Layout from "/src/components/Dashboard/Layout";
 import Platform from "/src/components/Layout/Platform";
 
+import { supabase } from "/src/utils/SupabaseClient";
 import { classMerge } from "/src/utils/TailwindUtilities";
+import { uniqueString } from "/src/utils/GenerateString";
 
 export default function Upload() {
-  const [selectedFile, setSelectedFile] = useState({
-    file: null,
-    map: null,
-  });
+  const [selectedFile, setSelectedFile] = useState({});
+  const [storageReceipt, setStorageReceipt] = useState({});
 
-  const addFile = (e) => {
-    if (
-      e.target.files[0].type !==
-      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-    ) {
-      e.target.value = null;
-      alert("Please upload an excel file");
-      return;
-    }
-
-    setSelectedFile({
-      ...selectedFile,
-      file: e.target.files[0],
-    });
-  };
-
-  const uploadPDS = (e) => {
+  const uploadPDS = async (e) => {
     e.preventDefault();
 
     if (selectedFile.file === null) {
@@ -37,14 +21,45 @@ export default function Upload() {
       return;
     }
 
-    return;
-  };
+    try {
+      const fileName = `${uniqueString(8)}.${
+        selectedFile.file.name.split(".")[1]
+      }`;
 
+      const { error: uploadError } = await supabase.storage
+        .from("pds-raw")
+        .upload(fileName, selectedFile.file);
+
+      if (uploadError) {
+        throw uploadError;
+      }
+
+      setStorageReceipt((prev) => ({
+        ...prev,
+        file_name: fileName,
+        default_name: selectedFile.file.name,
+      }));
+    } catch (error) {
+      alert("Supabase Storage: Error uploading file. See console for details.");
+      console.log(error);
+    }
+
+    const publicUrl = await supabase.storage
+      .from("pds-raw")
+      .getPublicUrl(storageReceipt.file_name);
+
+    setStorageReceipt((prev) => ({
+      ...prev,
+      file_url: publicUrl.data.publicUrl,
+    }));
+
+    // prepare payload for backend request
+  };
   return (
     <>
       <Tab.Group as={Fragment}>
         <Platform className="mt-10 mb-5">
-          <Tab.List className="flex items-center justify-between gap-1 p-1 bg-red-700 rounded-md">
+          <Tab.List className="flex items-center justify-between gap-1 rounded-md bg-red-700 p-1">
             <Tab as={Fragment}>
               {({ selected }) => (
                 <button
@@ -76,7 +91,7 @@ export default function Upload() {
           </Tab.List>
         </Platform>
         <Platform>
-          <Tab.Panels className="flex items-center justify-center p-2 bg-gray-200 rounded-md">
+          <Tab.Panels className="flex items-center justify-center rounded-md bg-gray-200 p-2">
             <Tab.Panel className="w-full">
               <div>
                 <h1 className="text-xl font-medium">Upload</h1>
@@ -85,19 +100,28 @@ export default function Upload() {
                 </p>
               </div>
               <div>
-                <form className="">
+                <form onSubmit={uploadPDS}>
                   <input
                     type="file"
-                    className="w-full p-4 mt-4 font-mono text-sm font-semibold rounded-md cursor-pointer hover:bg-gray-100"
+                    className="mt-4 w-full cursor-pointer rounded-md p-4 font-mono text-sm font-semibold hover:bg-gray-100"
                     onChange={(e) => {
-                      setSelectedFile({
-                        ...selectedFile,
+                      if (
+                        e.target.files[0].type !==
+                        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+                      ) {
+                        e.target.value = null;
+                        alert("Please upload an excel file");
+                        return;
+                      }
+
+                      setSelectedFile((prev) => ({
+                        ...prev,
                         file: e.target.files[0],
-                      });
+                      }));
                     }}
                   />
 
-                  <div className="flex items-center justify-end w-full mt-2 gap-x-2">
+                  <div className="mt-2 flex w-full items-center justify-end gap-x-2">
                     <button
                       type="reset"
                       className={classMerge(
@@ -111,13 +135,6 @@ export default function Upload() {
                       className={classMerge(
                         "rounded-md bg-white p-2 text-sm font-semibold uppercase hover:bg-green-500"
                       )}
-                      onClick={(e) => {
-                        e.preventDefault();
-                        if (selectedFile.file === null) {
-                          alert("Please select a file");
-                          return;
-                        }
-                      }}
                     >
                       Upload
                     </button>
